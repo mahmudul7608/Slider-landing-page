291c0e78dd5d49f9a43f1e9ca65f4a78d2099e21
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-12-07 01:47:15 +0600

    Change all think and add video slider and js

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
+  loop: true,
 
-function currentDiv(n) {
-  showDivs((slideIndex = n));
-}
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " w3-white";
-}
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
